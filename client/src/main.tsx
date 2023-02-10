c3aa2fc4c271763e2d084c62220ccba4410bc879
Anderson <developersanter@gmail.com>
2023-02-10 08:47:45 -0300

    Vitinho Deus
    </React.StrictMode>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/page/App'
 import "./i18n/translation"
+console.log('teste')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
